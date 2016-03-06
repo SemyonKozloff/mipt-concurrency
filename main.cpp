ab425006d2768448ede68a13fa0861b7206a9fe9
Semyon <semkozloff@gmail.com>
2016-03-06 17:13:19 +0300

    Test commit.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <future>
+#include <algorithm>
+#include <cassert>
+
+#include <unistd.h>
+
+// std::thread
+// std::mutex;
+// std::recursive_mutex;
+// std::lock_guard
+// std::unique_lock
+// std::atomic
+// std::atomic_flag
+
+// std::future
+// std::promise
+// std::async
+// std::package_task
+// std::async
+
+int main()
+{
+}
